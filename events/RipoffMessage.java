60d04e8778edc2ac6141e795146ffd978509590c
Tyler Wallschleger <TylerNW3994@gmail.com>
2019-11-11 13:30:02 -0500

    Reverted back to old files from old branch

--- events/RipoffMessage.java
+++ events/RipoffMessage.java
@@ -5,19 +5,32 @@ package events;
 
 /**
  *
- * @author tnwallsc and I guess Ike helped here too.
+ * @author tnwallsc
  */
 public class RipoffMessage {
     
+    /*
+    * UI Messages.
+    */
     public static final int GAME_PANEL = 1;
     public static final int PLAYER_PANEL = 2;
-    public static final int SEE_DECK = 2;
-    public static final int SEND_MEMES = 3;
-    public static final int HOW_TO_PLAY = 4;
-    public static final int PLAY_AGAINST_HOOMANS = 5;
-    public static final int PLAY_AGAINST_AI = 6;
-    public static final int EXIT_PANEL = 7;
-    public static final int PLAYER_WON = 8;
+    public static final int EXIT_PANEL = 3;
+    public static final int REGISTER_PANEL = 4;
+    
+    /*
+    * Player Messages.
+    */
+    public static final int SEE_DECK = 1;
+    public static final int SEND_MEMES = 2;
+    public static final int CREATE_PROFILE = 3;
+    
+    /*
+    * Game Messages.
+    */
+    public static final int HOW_TO_PLAY = 1;
+    public static final int PLAY_AGAINST_HOOMANS = 2;
+    public static final int PLAY_AGAINST_AI = 3;
+    public static final int PLAYER_WON = 4;
     
     private final int _thismessage;
     
